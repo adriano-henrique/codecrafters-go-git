9a74c2d13f5aca2fad317190506e1c4b103b0b59
root <root@localhost>
2024-07-02 20:20:57 +0000

    Adding implementation for cat-file

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
@@ -26,6 +29,36 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		if len(os.Args) < 3 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file <object> <path>\n")
+			os.Exit(1)
+		}
+		firstTwo := os.Args[3][:2]
+		restChar := os.Args[3][2:]
+		path := fmt.Sprintf(".git/objects/%s/%s", firstTwo, restChar)
+		file, err := os.Open(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		r, err := zlib.NewReader(io.Reader(file))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating zlib reader: %s\n", err)
+			os.Exit(1)
+		}
+		defer r.Close()
+
+		s, err := io.ReadAll(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading from zlib reader: %s\n", err)
+			os.Exit(1)
+		}
+
+		parts := strings.Split(string(s), "\x00")
+		fmt.Print(parts[1])
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
